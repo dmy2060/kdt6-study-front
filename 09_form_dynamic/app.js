6c56eff05adfc7a997a2912fe82d53368048c38f
fkdslfjldskfjsd <dmy2060@naver.com>
2023-03-30 17:40:51 +0900

    study:동적 form 생성 프로젝트 만듬

---
+++ 09_form_dynamic/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.set("view engine", "ejs");
+app.use("/views", express.static(__dirname + "/views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log("09_form_dynamic 서버 실행");
+  console.log(`http://localhost:${PORT}`);
+});
