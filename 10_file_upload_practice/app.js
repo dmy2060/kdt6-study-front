7cd8346d23364125805bafd3ce02ff576fed4997
fkdslfjldskfjsd <dmy2060@naver.com>
2023-04-01 11:54:33 +0900

    study:파일 업로드 실습

---
+++ 10_file_upload_practice/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+const multer = require("multer");
+const path = require("path");
+
+const uploadDetail = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(req.body.id, ext) + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+app.set("view engine", "ejs");
+app.use("/views", express.static(__dirname + "/views"));
+app.use("/static", express.static(__dirname + "/static"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/result", uploadDetail.single("userfile"), (req, res) => {
+  console.log(req.file);
+  console.log(req.body);
+  res.render("result", {
+    file: req.file,
+    userInfo: req.body,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("10_file_upload_practice 서버 실행");
+  console.log(`http://localhost:${PORT}`);
+});
