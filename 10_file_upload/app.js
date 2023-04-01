b625473acdaa829a4b2a37ac490e06ed7a6736f7
fkdslfjldskfjsd <dmy2060@naver.com>
2023-04-01 10:39:21 +0900

    study: multer 여러개 파일 업로드

--- 10_file_upload/app.js
+++ 10_file_upload/app.js
@@ -5,8 +5,30 @@ const PORT = 8000;
 // multer 미들웨어 사용하기
 const multer = require("multer"); // multer 불러오기
 const path = require("path"); // path 불러오기 (내장 모듈) => 파일, 폴더 경로를 쉽게 설정
-const upload = multer({
-  dest: "uploads/", // dest: 업로드할 파일 경로 지정
+// const upload = multer({
+//   dest: "uploads/", // dest: 업로드할 파일 경로 지정
+// });
+// multer 세부 설정
+const uploadDetail = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      // destination: 경로 설정
+      // done: callback 함수
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      // 가정) apple.png 파일을 업로드
+      const ext = path.extname(file.originalname); // file.originalname에서 "확장자" 추출 => png
+      // path.basename(file.originalname, ext) => apple (확장자 제거한 파일이름만!!)
+      // Date.now() => 현재 시간 (1680309346279)
+      // => 1970년 1월 1일 0시 0분 0초를 기준으로 현재까지 경과된 밀리초
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+      // [파일명 + 현재시간.확장자] 형식으로 파일 업로드
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB로 파일 크기 제한
+  // 5 * 2^10 = 5KB
+  // 5 * 2^10 * 2^10 = 5MB
 });
 
 app.set("view engine", "ejs");
@@ -21,7 +43,7 @@ app.get("/", (req, res) => {
 
 // single(): 하나의 파일 업로드할 때
 // single()의 매개변수: input의 name과 일치시키기!
-app.post("/upload", upload.single("userfile"), (req, res) => {
+app.post("/upload", uploadDetail.single("userfile"), (req, res) => {
   console.log(req.file); // 업로드한 파일 정보는 여기 존재
   /* 
   {
@@ -41,6 +63,24 @@ app.post("/upload", upload.single("userfile"), (req, res) => {
   */
   res.send("upload 완료~!!");
 });
+
+// array(): 여러 파일을 한 번에 업로드할 때 사용
+app.post("/upload/array", uploadDetail.array("userfile"), (req, res) => {
+  console.log(req.files); // [{}, {}, {}]
+  console.log(req.body); // { title : 'xxx' }
+  res.send("여러개 파일 업로드 완료~!");
+});
+
+// fields(): 여러 파일을 각각 input에 업로드할 때 사용
+app.post(
+  "/upload/fields",
+  uploadDetail.fields([{ name: "userfile1" }, { name: "userfile2" }]),
+  (req, res) => {
+    console.log(req.files); // { userfile1:[{}], userfile2:[{}] } 형태로 파일 정보 출력
+    console.log(req.body); // { title1: '냥1', title2: '냥2' }
+    res.send("각각 여러 파일 업로드 완료~!");
+  }
+);
 
 app.listen(PORT, () => {
   console.log("10_file_upload 서버 실행");
