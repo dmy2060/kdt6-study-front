3ae819eac57c6f1c8fb00073fe34b9d3e443b3ca
fkdslfjldskfjsd <dmy2060@naver.com>
2023-04-06 18:46:46 +0900

    study: mvc_mysql_delete

--- 13_mvc_mysql/routes/index.js
+++ 13_mvc_mysql/routes/index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const controller = require("../controller/Cvisitor");
 const router = express.Router();
 
+// GET / => localhost:PORT/
 router.get("/", controller.main);
+
+// GET /visitor => localhost:PORT/visitor
 router.get("/Visitor", controller.getVisitors); // 전체 조회
 
 // POST /visitor/write
 router.post("/visitor/write", controller.postVisitor); // 하나 추가
+
+// DELETE /visitor/delete
+router.delete("/visitor/delete", controller.deleteVisitor); // 하나 삭제
+
 module.exports = router;
