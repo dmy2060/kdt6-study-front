70d3b2a34d46130241fba902ca94d578dcf6783f
fkdslfjldskfjsd <dmy2060@naver.com>
2023-04-20 17:29:39 +0900

    study: 리액트 합성이벤트

--- 17_react_distruct/react-basic/src/ChangeText.js
+++ 17_react_distruct/react-basic/src/ChangeText.js
@@ -26,3 +26,25 @@ const ChangeText = () => {
 };
 
 export default ChangeText;
+
+// 실습 풀이
+// import { useState } from 'react';
+
+// const Prob2 = () => {
+//   const [visible, setVisible] = useState(true);
+
+//   const toggle = () => {
+//     setVisible(!visible);
+//     // !true => flase
+//     // !fale => true
+//   };
+
+//   return (
+//     <>
+//       <button onClick={toggle}>{visible ? ' 사라져라!' : '보여라!'}</button>
+//       <h1>{visible && '안녕하세요'}</h1>
+//     </>
+//   );
+// };
+
+// export default Prob2;
---
+++ 22_react_04_eventhandling/src/SyntheticEvent.js
@@ -0,0 +1,25 @@
+function SyntheticEvent() {
+  function syntheticEvent(e) {
+    // SyntheticEvent (합성 이벤트)
+    // 웹 브라우저의 nativeEvent가 아니라
+    // nativeEvent를 감싼 Synthetic Event를 사용함
+    // 즉, 리액트 고유 이벤트 객체!
+    console.log(e);
+  }
+  function printInputValue(e) {
+    // console.log(e);
+    console.log(e.target.value);
+  }
+  return (
+    <div>
+      <button onClick={syntheticEvent}>합성이벤트 콘솔 찍기</button>
+      <input
+        type="text"
+        placeholder="아무거나 입력!"
+        onChange={printInputValue}
+      />
+    </div>
+  );
+}
+
+export default SyntheticEvent;
