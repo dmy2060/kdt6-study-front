f1b238c5b66ed313683b9f4ccacfa0eb55e9cb4e
fkdslfjldskfjsd <dmy2060@naver.com>
2023-04-23 17:13:27 +0900

    study:react useRef

--- 24_react_06_ref_lifecycle/src/App.js
+++ 24_react_06_ref_lifecycle/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import RefSample1 from "./RefSample1";
+// import RefSample2 from "./RefSample2";
+// import RefSample3 from "./RefSample3";
+// import RefSample4 from "./RefSample4";
+import Prob1 from "./Prob1";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Prob1 />
     </div>
   );
 }
