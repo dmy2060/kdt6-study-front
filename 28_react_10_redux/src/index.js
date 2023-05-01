a302d677854fe7ff98648442d93a3dc7d838c1cd
fkdslfjldskfjsd <dmy2060@naver.com>
2023-05-01 17:32:20 +0900

    study: react redux 구조화

---
+++ 28_react_10_redux/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import App4 from "./App4";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./store";
+
+// store 정의 : 상태를 관리하는 공간!
+// 단, 하나의 프로젝트에는 하나의 store만 있어야 함!
+const store = configureStore({ reducer: rootReducer }, composeWithDevTools());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // Provider 컴포넌트: store props를 반드시 가져야 함
+  // Provider 하위 컴포넌트들은 store에 저장된 state들을 사용 가능!
+  <Provider store={store}>
+    <App4 />
+  </Provider>
+);
---
+++ 28_react_10_redux/src/store/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from "redux";
+import counterReducer from "./counterReducer";
+import isDataReducer from "./isDataReducer";
+// combineReducers : 여러 개의 리듀서를 하나로 합치는 일을 함
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  isData: isDataReducer,
+});
+
+// 다른 파일에서 여러 리듀서를 합친 최종 리듀서(rootReducer)
+// 를 사용할 수 있도록 내보냄
+
+export default rootReducer;
